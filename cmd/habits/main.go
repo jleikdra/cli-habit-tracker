0a811f7f48c378b8914eee37c68c2158873bb208
Johnny Leikvoll Drabløs <johnny.leikvoll@gmail.com>
2025-11-10 12:13:16 +0100

    added update command

--- cmd/habits/main.go
+++ cmd/habits/main.go
@@ -13,41 +13,48 @@ func main() {
 	command := os.Args[1]
 
 	switch command {
-		case "help":
-			fmt.Println("Commands:")
-			fmt.Printf("  %-20s %s\n", "habit add [name]", "Add a new habit")
-			fmt.Printf("  %-20s %s\n", "habit do  [name]", "Mark a habit as done")
-			fmt.Printf("  %-20s %s\n", "habit rm  [name]", "Remove a habit")
-			fmt.Printf("  %-20s %s\n", "habit ls", "List all habits")
+	case "help":
+		fmt.Println("Commands:")
+		fmt.Printf("  %-20s %s\n", "habit add [name]", "Add a new habit")
+		fmt.Printf("  %-20s %s\n", "habit do  [name]", "Mark a habit as done")
+		fmt.Printf("  %-20s %s\n", "habit rm  [name]", "Remove a habit")
+		fmt.Printf("  %-20s %s\n", "habit upd [old] [new]", "Update a habit name")
+		fmt.Printf("  %-20s %s\n", "habit ls", "List all habits")
 
-		case "add":
-			if len(os.Args) < 3 {
-				fmt.Println("Error. Habit name required.")
-				os.Exit(1)
-			}
-			fmt.Println("Habit added")
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Error. Habit name required.")
+			os.Exit(1)
+		}
+		fmt.Println("Habit added")
 
-		case "do":
-			if len(os.Args) < 3 {
-				fmt.Println("Error. Habit name required.")
-				os.Exit(1)
-			}
-			fmt.Println("Habit marked as done")
+	case "do":
+		if len(os.Args) < 3 {
+			fmt.Println("Error. Habit name required.")
+			os.Exit(1)
+		}
+		fmt.Println("Habit marked as done")
 
-		case "rm":
-			if len(os.Args) < 3 {
-				fmt.Println("Error. Habit name required.")
-				os.Exit(1)
-			}
-			fmt.Println("Habit removed")
+	case "rm":
+		if len(os.Args) < 3 {
+			fmt.Println("Error. Habit name required.")
+			os.Exit(1)
+		}
+		fmt.Println("Habit removed")
 
-		case "ls":
-			fmt.Println("Habit list")
+	case "upd":
+		if len(os.Args) < 4 {
+			fmt.Println("Error. Old and new habit names required.")
+			os.Exit(1)
+		}
+		fmt.Println("Habit updated")
 
-		default:
-			fmt.Printf("Unknown command: %s\n", command)
-			fmt.Println("Run habit help for instructions.")
+	case "ls":
+		fmt.Println("Habit list")
+
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+		fmt.Println("Run habit help for instructions.")
 	}
 
-}
 }
